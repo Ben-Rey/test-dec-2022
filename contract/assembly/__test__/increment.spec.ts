4a87a6f7e2d92ea6ebef997e389d58c396644be4
fen|x <benjamin.reynes.dev@gmail.com>
2022-12-27 07:38:07 +0100

    Improve test

---
+++ contract/assembly/__test__/increment.spec.ts
@@ -0,0 +1,38 @@
+// Test only owner
+// Test only Is init
+// Test overflow
+// Test Ingrement
+// Test get Value
+
+import { init, increment, get_value } from "../contracts/main";
+import { Args, fromBytes } from "@massalabs/massa-as-sdk";
+
+const OWNER_ADDRESS = "S1273BfFRp7B2ELtMLG4yibtad3QHgRTjJrNdD8LDUjS9mB2wQJM";
+const USER_ADDRESS = "S1bXjyPwrssNmG4oUG5SEqaUhQkVArQi7rzQDWpCprTSmEgZDGG";
+const NUM_TO_ADD = 3;
+const INITIAL_COUNTER_VAlUE = 0;
+
+describe("Increment Function", (): i32 => {
+    test("Should Init Counter to 0", (): i32 => {
+        init([]);
+        const num = parseInt(fromBytes(get_value([])));
+        if (assertNumIsEqual(num, INITIAL_COUNTER_VAlUE)) return TestResult.Success;
+        else return TestResult.Failure;
+    });
+    test("Should succed if Counter incremented by a number", (): i32 => {
+        init([]);
+        increment(new Args().add(NUM_TO_ADD).serialize());
+        const num = parseInt(fromBytes(get_value([])));
+        if (assertNumIsEqual(num, NUM_TO_ADD)) return TestResult.Success;
+        else return TestResult.Failure;
+    });
+    return TestResult.Success;
+});
+
+function assertNumIsEqual(num: f64, equalTo: f64): boolean {
+    if (num !== equalTo) {
+        assert(num === equalTo, `FAILURE :: Num should be equal to ${equalTo} => num is equal to ${num}`);
+        return false;
+    }
+    return true;
+}
